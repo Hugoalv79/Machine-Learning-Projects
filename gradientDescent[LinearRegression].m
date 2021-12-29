793f35238c452d44fce7c3f7a07a316024ab3346
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-12-28 21:03:35 -0600

    Update and rename gradientDescent.m to gradientDescent[LinearRegression].m

--- gradientDescent[LinearRegression].m
+++ gradientDescent[LinearRegression].m
@@ -16,12 +16,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    
+    hypothesis = X * theta;
+    
+    error = hypothesis - y;
 
-
-
-
-
-
+    thetaChange = alpha * (1/m) * X' * error;
+        
+    theta = theta - thetaChange;
 
     % ============================================================
 
